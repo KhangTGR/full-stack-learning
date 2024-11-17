9a412b1e4974b52d9571a1cd974f6adb11911aba
KhangLeonardo <s3929994@rmit.edu.vn>
2024-11-17 06:57:22 +0000

    remove variables from main.tf

--- main.tf
+++ main.tf
@@ -47,10 +47,10 @@ module "deployment" {
   environment = var.environment
 
   # Load Balancer
-  alb_blue_target_group_name  = "khang-sample-tg-blue"
-  alb_green_target_group_name = "khang-sample-tg-green"
-  alb_prod_listener_arns      = ["arn:aws:elasticloadbalancing:ap-southeast-1:879654127886:listener/app/khang-sample-app-alb/95ad5c51b27e5e8b/a1411ba023e74163"]
-  alb_test_listener_arns      = []
+  alb_blue_target_group_name  = var.alb_blue_target_group_name
+  alb_green_target_group_name = var.alb_green_target_group_name
+  alb_prod_listener_arns      = var.alb_prod_listener_arns
+  alb_test_listener_arns      = var.alb_test_listener_arns
 
   # Container
   container_name = "khang-sample-app"
@@ -58,8 +58,8 @@ module "deployment" {
 
   # ECS
   ecs_cluster_name = module.cluster.cluster_name
-  ecs_service_name = "khang-sample-app-service"
-  ecs_task_def_arn = "arn:aws:ecs:ap-southeast-1:879654127886:task-definition/khang-sample-app-taskdef:7"
+  ecs_service_name = var.ecs_service_name
+  ecs_task_def_arn = var.ecs_task_def_arn
 
   # Others
   old_tasks_termination_wait_time = 5
